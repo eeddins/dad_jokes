137abb9713f107dbb02480fc80f5925b4c233a96
Eddins, Eric A <eeddins@gmail.com>
2024-01-03 14:27:47 -0600

    added scraper and temp jokes

--- src/daily_dad_jokes.py
+++ src/daily_dad_jokes.py
@@ -2,11 +2,15 @@ import json
 import random
 
 # Open the file and load the JSON data
-with open("../dadjokes.list", "r") as f:
+with open("dadjokes.list", "r") as f:
     jokes = json.load(f)
 
 # Pick a random joke from the list
 random_joke = random.choice(jokes)
 
 # Print the joke
-print(random_joke)
+# print(random_joke)
+
+# Save the joke to a file
+with open("jod.txt", "w") as f:
+    f.write(random_joke)
